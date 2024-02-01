f5bcfc27dc8b4fcf6812a508621b57029f380d9a
VithushanRavikrishnan <ravivithu1997@gmail.com>
2024-02-01 13:50:58 +0530

    Create Sign Up UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,14 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import Header from './components/Header';
+
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/dashboard' element={<Dashboard/>} />
