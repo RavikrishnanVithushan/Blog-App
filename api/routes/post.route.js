1749268ab494185808d7c351a5261806e3e12efd
VithushanRavikrishnan <ravivithu1997@gmail.com>
2024-02-14 15:54:19 +0530

    Update post functionality completed

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {verifyToken} from '../utils/verifyUser.js'
-import { createPost,getPosts,deletePost } from '../controllers/post.controller.js';
+import { createPost,getPosts,deletePost, updatepost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create-post',verifyToken,createPost)
 router.get('/get-posts',getPosts);
-router.delete('/delete-post/:postId/:userId',verifyToken,deletePost)
+router.delete('/delete-post/:postId/:userId',verifyToken,deletePost);
+router.put('/update-post/:postId/:userId',verifyToken,updatepost);
 
 export default router;
