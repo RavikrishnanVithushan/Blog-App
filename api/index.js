20c98c44d3b6eddf408fdeaa8027483df945b3c2
VithushanRavikrishnan <ravivithu1997@gmail.com>
2024-02-16 14:11:19 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -21,6 +22,9 @@ mongoose
     confirm.log(err)
   });
 
+
+const __dirname =path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -34,6 +38,12 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 
 app.use((err,req,res,next)=>{
 
