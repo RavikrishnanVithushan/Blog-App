2ceb6cfd3d8e999c792c6972777f242caa0f843f
VithushanRavikrishnan <ravivithu1997@gmail.com>
2024-02-16 10:11:02 +0530

    create get all users api

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -9,4 +9,5 @@ router.get('/get-posts',getPosts);
 router.delete('/delete-post/:postId/:userId',verifyToken,deletePost);
 router.put('/update-post/:postId/:userId',verifyToken,updatepost);
 
+
 export default router;
